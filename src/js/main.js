97859f57ba761a25f01acbe1586e224062de335a
Jermaine Maragh <maragh@users.noreply.github.com>
2024-03-31 16:30:37 -0700

    create basic vue app to delete public repo in bulk

---
+++ src/js/main.js
@@ -0,0 +1,53 @@
+import * as bootstrap from 'bootstrap'
+import { createApp, reactive } from "vue/dist/vue.esm-bundler.js"
+import 'dotenv/config'
+import _ from 'lodash'
+
+import api from './api'
+
+globalThis.__VUE_OPTIONS_API__ = process.env.NODE_ENV === "development"
+globalThis.__VUE_PROD_DEVTOOLS__ = process.env.NODE_ENV === "development"
+globalThis.__VUE_PROD_HYDRATION_MISMATCH_DETAILS__ = process.env.NODE_ENV === "development"
+
+const App = {
+    created(){
+        console.log(`environment: ${process.env.NODE_ENV}`)
+    },
+    mounted(){
+        this.load_repos()
+    },
+    data(){
+        return {
+            repositories: [],
+            repository: []
+        }
+    },
+    methods:{
+        load_repos: function(){
+            api.fetch_repositories().then(data => {
+                this.repositories = data
+                console.log(data)
+            }).catch(error => {
+                console.log(error)
+            })
+        },
+        delete_repo: function(repo){
+            api.delete_repository(repo).then(response => {
+                console.log(response)
+            }).catch(error => {
+                console.error(error)
+            })
+
+        },
+        delete_repository: function(){
+            _.map(this.repository, this.delete_repo)
+            this.repository = []
+            this.repositories = []
+            this.load_repos()
+
+        }
+    }
+}
+
+
+createApp(App).mount("#app")
